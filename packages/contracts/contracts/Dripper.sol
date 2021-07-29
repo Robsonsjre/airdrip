fb565ea9a61b0d278188cfc357bcaffa23fe93cc
Guilherme Viana <ggviana@hotmail.com.br>
2021-07-29 10:00:37 -0300

    Fixed contracts

--- packages/contracts/contracts/Dripper.sol
+++ packages/contracts/contracts/Dripper.sol
@@ -14,124 +14,119 @@ import "./DripToken.sol";
  * liquidity programs from dumps
  */
 contract Dripper {
-    using SafeERC20 for IERC20;
-    using SafeERC20 for IERC20Metadata;
+  using SafeERC20 for IERC20;
+  using SafeERC20 for IERC20Metadata;
 
-    uint public constant MIN_GAP_TIME = 24*60*60;
+  uint public constant MIN_GAP_TIME = 24 * 60 * 60;
 
-    /**
-     * @notice The campaigns objects identifiable by their DripToken address.
-     */
-    struct Campaign {
-        uint streamId;
-        address option;
-    }
+  /**
+   * @notice The campaigns objects identifiable by their DripToken address.
+   */
+  struct Campaign {
+    uint streamId;
+    address option;
+  }
 
-    /**
-     * @notice The campaigns objects identifiable by their DripToken address.
-     */
-    mapping(address => Campaign) private _campaigns;
+  /**
+   * @notice The campaigns objects identifiable by their DripToken address.
+   */
+  mapping(address => Campaign) private _campaigns;
 
-    /**
-     * @notice Pods entrypoint to create options.
-     */
-    address private immutable _configurationManager;
+  /**
+   * @notice Pods entrypoint to create options.
+   */
+  address private immutable _configurationManager;
 
-    /**
-     * @notice Sablier stream creator
-     */
-    address private immutable _sablier;
+  /**
+   * @notice Sablier stream creator
+   */
+  address private immutable _sablier;
 
-    event CampaignCreated(address indexed creator, address campaignId);
+  event CampaignCreated(address indexed creator, address campaignId);
 
-    constructor(address configurationManager_, address sablier_) {
-        _configurationManager = configurationManager_;
-        _sablier = sablier_;
-    }
+  constructor(address configurationManager_, address sablier_) {
+    _configurationManager = configurationManager_;
+    _sablier = sablier_;
+  }
 
-    /**
-     * @dev Returns the address of Pods ConfigurationManager used to create options.
-     */
-    function configurationManager() public view returns(address) {
-        return _configurationManager;
-    }
+  /**
+   * @dev Returns the address of Pods ConfigurationManager used to create options.
+   */
+  function configurationManager() public view returns (address) {
+    return _configurationManager;
+  }
 
-    /**
-     * @dev Returns the address of Sablier master contract used to create streams.
-     */
-    function sablier() public view returns(address) {
-        return _sablier;
-    }
+  /**
+   * @dev Returns the address of Sablier master contract used to create streams.
+   */
+  function sablier() public view returns (address) {
+    return _sablier;
+  }
 
-    /**
-     * @dev Returns the Campaign associated with a DripToken.
-     */
-    function getCampaign(address drip) public view returns(Campaign memory) {
-        return _campaigns[drip];
-    }
+  /**
+   * @dev Returns the Campaign associated with a DripToken.
+   */
+  function getCampaign(address drip) public view returns (Campaign memory) {
+    return _campaigns[drip];
+  }
 
-    /**
-     * @dev Create a campaign
-     */
-    function createCampaign(
-        IERC20Metadata underlyingAsset,
-        IERC20Metadata strikeAsset,
-        uint campaignAmount,
-        uint strikePrice,
-        uint expiration,
-        uint startTime,
-        uint endTime
-    ) public returns (address) {
-        require(endTime - expiration > MIN_GAP_TIME, "invalid endTime");
-        underlyingAsset.safeTransferFrom(msg.sender, address(this), campaignAmount);
+  /**
+   * @dev Create a campaign
+   */
+  function createCampaign(
+    IERC20Metadata underlyingAsset,
+    IERC20Metadata strikeAsset,
+    uint campaignAmount,
+    uint strikePrice,
+    uint expiration,
+    uint startTime,
+    uint endTime
+  ) public returns (address) {
+    require(endTime - expiration > MIN_GAP_TIME, "invalid endTime");
+    underlyingAsset.safeTransferFrom(msg.sender, address(this), campaignAmount);
 
-        IPodOption option = _createOption(underlyingAsset, strikeAsset, strikePrice, expiration);
+    IPodOption option = _createOption(underlyingAsset, strikeAsset, strikePrice, expiration);
+    underlyingAsset.approve(address(option), campaignAmount);
+    option.mint(campaignAmount, address(this));
 
-        underlyingAsset.approve(address(option), campaignAmount);
+    DripToken drip = new DripToken(this);
+    drip.mint(msg.sender, campaignAmount);
 
-        option.mint(campaignAmount, address(this));
+    option.approve(address(_sablier), campaignAmount);
+    uint streamId = ISablier(_sablier).createStream(address(drip), campaignAmount, address(option), startTime, endTime);
 
-        option.approve(address(sablier), campaignAmount);
+    _campaigns[address(drip)] = Campaign({
+      option : address(option),
+      streamId : streamId
+    });
 
-        DripToken drip = new DripToken(this);
-        drip.mint(msg.sender, underlyingAmount);
+    emit CampaignCreated(msg.sender, address(drip));
+    return address(drip);
+  }
 
-        uint streamId = ISablier(_sablier).createStream(address(drip), campaignAmount, address(option), startTime, endTime);
-
-
-        _campaigns[address(drip)] = Campaign({
-            option: address(option),
-            streamId: streamId
-        });
-
-        drip.mint(msg.sender, campaignAmount);
-        emit CampaignCreated(msg.sender, address(drip));
-        return address(drip);
-    }
-
-    /**
-     * @dev Create a Option on Pods Finance
-     */
-    function _createOption(
-        IERC20Metadata underlyingAsset,
-        IERC20Metadata strikeAsset,
-        uint strikePrice,
-        uint expiration
-    ) internal returns (IPodOption) {
-        IOptionFactory optionFactory = IOptionFactory(IConfigurationManager(_configurationManager).getOptionFactory());
-        return IPodOption(
-            optionFactory.createOption(
-                string(abi.encodePacked(underlyingAsset.symbol(), " Call Option")),
-                string(abi.encodePacked("Pod", underlyingAsset.symbol(), ":", strikeAsset.symbol())),
-                IPodOption.OptionType.CALL,
-                IPodOption.ExerciseType.AMERICAN,
-                address(underlyingAsset),
-                address(strikeAsset),
-                strikePrice,
-                expiration,
-                0,
-                false
-            )
-        );
-    }
+  /**
+   * @dev Create a Option on Pods Finance
+   */
+  function _createOption(
+    IERC20Metadata underlyingAsset,
+    IERC20Metadata strikeAsset,
+    uint strikePrice,
+    uint expiration
+  ) internal returns (IPodOption) {
+    IOptionFactory optionFactory = IOptionFactory(IConfigurationManager(_configurationManager).getOptionFactory());
+    return IPodOption(
+      optionFactory.createOption(
+        string(abi.encodePacked(underlyingAsset.symbol(), " Call Option")),
+        string(abi.encodePacked("Pod", underlyingAsset.symbol(), ":", strikeAsset.symbol())),
+        IPodOption.OptionType.CALL,
+        IPodOption.ExerciseType.AMERICAN,
+        address(underlyingAsset),
+        address(strikeAsset),
+        strikePrice,
+        expiration,
+        0,
+        false
+      )
+    );
+  }
 }
